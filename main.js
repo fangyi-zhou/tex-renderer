bddb10acec801101a03c8b4a67727c85613ff999
Fangyi Zhou <me@fangyi.io>
2020-01-13 19:36:39 +0000

    Fix a few things

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const tmp = require('tmp-promise')
 const child_process = require('child-process-promise')
 const fs = require('fs');
 const process = require('process');
+const git = require('nodegit');
 
 function is_valid({ github_name, github_repo, root }) {
     const safe_chars = /^[A-Za-z-_0-9.]+$/;
@@ -12,13 +13,14 @@ function is_valid({ github_name, github_repo, root }) {
 }
 
 async function render(github_name, github_repo, root) {
-    const tmpdir = await tmp.dir();
-    const git_clone = `git clone https://github.com/${github_name}/${github_repo} .`;
+    const tmpdir = await tmp.dir({unsafeCleanup: true});
+    const repo = `https://github.com/${github_name}/${github_repo}`;
+    await git.Clone(repo, tmpdir.path);
     const latexmk = `latexmk -pdf ${root}`;
     const output_file = path.join(tmpdir.path, path.basename(root, ".tex") + ".pdf");
-    const cmd = `cd ${tmpdir.path} && ${git_clone} && ${latexmk}`;
+    const cmd = `cd ${tmpdir.path} && ${latexmk}`;
     console.log(cmd);
-    await child_process.exec(cmd, { timeout: 60 });
+    await child_process.exec(cmd, { timeout: 60000 });
     const output = await fs.promises.readFile(output_file);
     tmpdir.cleanup();
     return output;
